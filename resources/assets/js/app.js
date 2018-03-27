7de64a6febfc4896b5d7b4341448768e2d1c005e
bgcert <register@bgcert-training.com>
2018-03-27 13:28:45 +0300

    Element UI installed and working!

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/bg'
+
+Vue.use(ElementUI, { locale })
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
