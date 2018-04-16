73c277732a4581e1c5ef75f289f544418d08511c
bgcert <register@bgcert-training.com>
2018-04-16 19:18:00 +0300

    Venue profil improved

--- resources/views/venue.blade.php
+++ resources/views/venue.blade.php
@@ -2,20 +2,93 @@
 
 @section('content')
 <div class="container">
-	<h1>Venue</h1>
-	<hr>
-	<img src="{{ $venue->cover }}" style="height: 300px;">
+	<div class="ui hidden divider"></div>
+	<div class="ui grid">
+		<div class="row">
+			<div class="twelve wide column">
+				<div class="cover" style="background: url({{ $venue->cover }}) center / cover no-repeat; height: 400px;">
+					
+				</div>
+				<div class="ui small images">
+					@foreach($images as $image)
+					<img src="{{ $image->filename }}">
+					@endforeach
+				</div>
+				<h4 class="ui dividing header">
+					ПРЕПОРЪЧАНИ ЗАЛИ
+				</h4>
+				<div class="ui three column grid">
+					<div class="column">
+						<div class="item">
+							<a class="ui small image">
+								<img src="https://picsum.photos/600/400/?image=112">
+							</a>
+							<div class="content">
+								<a class="header">Cute Dog</a>
+								<div class="description">
+									<p>Many people also have their own barometers for what makes a cute dog.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="column">
+						<div class="item">
+							<a class="ui small image">
+								<img src="https://picsum.photos/600/400/?image=112">
+							</a>
+							<div class="content">
+								<a class="header">Cute Dog</a>
+								<div class="description">
+									<p>Many people also have their own barometers for what makes a cute dog.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="column">
+						<div class="item">
+							<a class="ui small image">
+								<img src="https://picsum.photos/600/400/?image=112">
+							</a>
+							<div class="content">
+								<a class="header">Cute Dog</a>
+								<div class="description">
+									<p>Many people also have their own barometers for what makes a cute dog.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="four wide column">
+				<h3 class="ui dividing header">
+					ДЕТАЙЛИ ЗА ЗАЛАТА
+				</h3>
+				<table class="ui very basic table">
+					<tbody>
+						<tr>
+							<td><b>Капацитет:</b></td>
+							<td>100 места</td>
+						</tr>
+						<tr>
+							<td><b> Кетеринг</b></td>
+							<td>Предлага</td>
+						</tr>
+						<tr>
+							<td><b> Мултимедия</b></td>
+							<td>Предлага</td>
+						</tr>
+					</tbody>
+				</table>
+				<button class="big ui positive basic fluid button">
+					Изпрати запитване
+				</button>
+			</div>
+		</div>
+	</div>
     <h2>{{ $venue->name }}</h2>
     <p>
     	{{ $venue->description }}
     </p>
-    <table>
-    	<tr>
-    		@foreach($images as $image)
-    			<td><img src="{{ $image->filename }}" style="width: 200px;"></td>
-    		@endforeach
-  		</tr>
-    </table>
     <p>
     	by <a href="/c/{{ $venue->company->slug }}">{{ $venue->company->name }}</a>
     </p>
