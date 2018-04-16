7e353db62c720181a8a3050659283ae2659d1e0a
bgcert <register@bgcert-training.com>
2018-04-16 16:53:28 +0300

    Minor improvements

--- database/factories/VenueFactory.php
+++ database/factories/VenueFactory.php
@@ -8,6 +8,6 @@ $factory->define(App\Venue::class, function (Faker $faker) {
         'name' => $faker->sentence($nbWords = 3, $variableNbWords = true),
         'description' => $faker->paragraph($nbSentences = 10, $variableNbSentences = true),
         'capacity' => $faker->numberBetween($min = 10, $max = 100),
-        'cover' => 'https://picsum.photos/600/400/?image=' . $faker->numberBetween($min = 100, $max = 1000),
+        'cover' => 'https://picsum.photos/800/400/?image=' . $faker->numberBetween($min = 100, $max = 400),
     ];
 });
--- database/factories/VenueImageFactory.php
+++ database/factories/VenueImageFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\VenueImage::class, function (Faker $faker) {
     return [
-        'filename' => 'https://picsum.photos/600/400/?image=' . $faker->numberBetween($min = 100, $max = 1000),
+        'filename' => 'https://picsum.photos/600/400/?image=' . $faker->numberBetween($min = 100, $max = 400),
     ];
 });
