fbb21feac04a54237c49592fd3a01f2816b1533c
bgcert <register@bgcert-training.com>
2018-05-10 17:10:35 +0300

    Semantic UI installations changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('node_modules/semantic-ui', 'resources/assets/dist/semantic-ui');
+   .sass('resources/assets/sass/app.scss', 'public/css');
