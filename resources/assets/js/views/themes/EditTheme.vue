5146f51afa2e1850801b2d492b98486e8b7bb808
bgcert <register@bgcert-training.com>
2018-05-23 15:58:08 +0300

    Working on image crop

--- resources/assets/js/views/themes/EditTheme.vue
+++ resources/assets/js/views/themes/EditTheme.vue
@@ -23,6 +23,14 @@
 						<el-input type="textarea" :rows="12" v-model="theme.body"></el-input>
 					</el-form-item>
 
+					<el-form-item label="Корица">
+						<input type="file" name="fileupload" @change="onFileSelected">
+						<!-- Wrap the image or canvas element with a block element (container) -->
+						<div>
+							<img id="image" src="https://picsum.photos/800/400/?image=194">
+						</div>
+					</el-form-item>
+
 					<el-form-item>
 						<div class="right floated">
 							<div class="ui right floated primary button" @click="save">
@@ -37,27 +45,18 @@
 					</el-form-item>
 				</el-form>
 
-				<h3>File upload</h3>
-				<hr>
-
-				<input type="file" @change="onFileChange">
-
-				<div id="canvas">
-					<img v-if="url" id="image" :src="url">
-				</div>
+				
 
 			</div>
 		</div>
 	</div>
 </template>
-
 <script>
     export default {
     	data: function () {
     		return {
-    			url: null,
-    			dialogVisible: false,
     			loading: true,
+    			selectedFile: null,
     			categories: [],
     			theme: [],
     			form: {
@@ -91,10 +90,29 @@
     			});
     		},
 
-    		onFileChange(e)
+    		onFileSelected(event)
     		{
-				const file = e.target.files[0];
-    			this.url = URL.createObjectURL(file);
+
+    			//$('#image').attr('src', URL.createObjectURL(event.target.files[0]));
+
+				var $image = $('#image');
+
+				$image.cropper({
+					aspectRatio: 16 / 9,
+					crop: function(event) {
+						console.log(event.detail.x);
+						console.log(event.detail.y);
+						console.log(event.detail.width);
+						console.log(event.detail.height);
+						console.log(event.detail.rotate);
+						console.log(event.detail.scaleX);
+						console.log(event.detail.scaleY);
+					}
+				});
+
+				// Get the Cropper.js instance after initialized
+				var cropper = $image.data('cropper');
+
     		}
     	},
 
@@ -103,6 +121,7 @@
         },
 
         created() {
+        	$.fn.cropper;
         	var vm = this;
             var route = '/dashboard/themes/' + this.$route.params.id + '/edit';
         	axios.get(route).then(function (response) {
@@ -119,8 +138,9 @@
 </script>
 
 <style>
-	#image img {
-		max-width: 400px;
+	/* Limit image width to avoid overflow the container */
+	img {
+	  max-width: 100%; /* This rule is very important, please do not ignore this! */
 	}
 
 </style>
