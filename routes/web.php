86ae167f00dbdfb593d198f4ea24ffbc3fbe4e20
bgcert <register@bgcert-training.com>
2018-06-14 16:35:56 +0300

    Messanger testing 4

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Auth::login(\App\User::find(1));
+//Auth::login(\App\User::find(1));
 
 Route::get('/eventtest', function () {
     $events = \App\Event::with('theme.likeCount', 'theme.commentCount', 'theme.isLiked', 'theme.company')->get();
