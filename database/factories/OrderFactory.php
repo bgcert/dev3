4fc1b794da118f1eaf7a1d65072637a4ea8dd759
bgcert <register@bgcert-training.com>
2018-06-23 18:24:39 +0300

    Remove qty from order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -6,7 +6,6 @@ $factory->define(App\Order::class, function (Faker $faker) {
     return [
         'user_id' => \App\User::all()->random()->id,
         'event_id' => \App\Event::all()->random()->id,
-        'qty' => $faker->numberBetween($min = 1, $max = 10),
         'invoice' => $faker->numberBetween($min = 0, $max = 1),
     ];
 });
