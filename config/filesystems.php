b94c5fa52928a15de2088caa15b98922d6b4f610
bgcert <register@bgcert-training.com>
2018-09-08 16:05:11 +0300

    Working on google cloud storage. Account changed to seminari365.ltd

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,9 +66,9 @@ return [
 
         'gcs' => [
 		    'driver' => 'gcs',
-		    'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'seminari365-189510'),
-		    'key_file' => base_path('seminari365-6b0eb9c5ecfd.json'), //env('GOOGLE_CLOUD_KEY_FILE', '/public/key.json'), 
-		    'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'seminari365-image-store'),
+		    'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'fifth-glazing-215809'),
+		    'key_file' => base_path('238c40786509.json'), //env('GOOGLE_CLOUD_KEY_FILE', '/public/key.json'), 
+		    'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 's365-test-bucket'),
 		    // 'base_url' => env('GCS_BASE_URL','your-base-url'),
 		    // 'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', 'http://storage.googleapis.com') // see: Public URLs below
 		],
