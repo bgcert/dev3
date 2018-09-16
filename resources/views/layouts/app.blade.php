e18500bef6fba70faaacf8f831405f2bf53c1737
bgcert <register@bgcert-training.com>
2018-09-16 17:30:04 +0300

    Testing desctop viewport 2.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <meta name="viewport" content="width=width=2000">
+    <meta name="viewport" content="width=width=1200">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
