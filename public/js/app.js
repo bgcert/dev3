4f0b4d3c48af5caf8b14637eb0263a7e09102bb2
bgcert <register@bgcert-training.com>
2018-11-15 17:45:21 +0200

    Dialogs mobile responsive.

--- public/js/app.js
+++ public/js/app.js
@@ -118518,37 +118518,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
--- resources/assets/js/components/auth/LoginModalComponent.vue
+++ resources/assets/js/components/auth/LoginModalComponent.vue
@@ -4,37 +4,6 @@
 
 		<el-dialog width="30%" title="Вход" :visible.sync="dialogFormVisible">
 			<login></login>
-			<!-- <el-form ref="form" label-position="left" label-width="140px" action="/">
-				
-				<el-form-item label="E-mail">
-					<template v-if="errorMessage.email" v-for="error in errorMessage.email">
-						<el-alert
-							:title="error"
-							type="error"
-							show-icon>
-						</el-alert>
-					</template>
-					<el-input type="text" v-model="form.email" name="email"></el-input>
-				</el-form-item>
-				<el-form-item label="Парола">
-					<template v-if="errorMessage.password" v-for="error in errorMessage.password">
-						<el-alert
-							:title="error"
-							type="error"
-							show-icon>
-						</el-alert>
-					</template>
-					<el-input type="password" v-model="form.password" name="password"></el-input>
-					<el-checkbox v-model="form.checked" name="remember"> Запомни ме</el-checkbox>
-				</el-form-item>
-
-				<el-form-item size="large">
-					<el-button @click.prevent="callRegister"> Регистрация</el-button>
-					<el-button type="primary" @click="onSubmit" :loading="loading"> Вход</el-button>
-					<el-button type="text" @click="callReset"> Забравена парола.</el-button>
-				</el-form-item>
-			</el-form> -->
-
 		</el-dialog>
 	</div>
 </template>
