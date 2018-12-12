8028bcdcf4b1b30c5d9271088b9fa229d9dd830d
bgcert <register@bgcert-training.com>
2018-12-12 13:56:02 +0200

    Contact problem fixed.

--- resources/assets/js/components/ContactPublisherComponent.vue
+++ resources/assets/js/components/ContactPublisherComponent.vue
@@ -34,64 +34,12 @@
 						<div class="form-group text-right">
 							<button class="btn btn-outline-secondary" @click.prevent="closeModal">Откажи</button>
 							<button class="btn btn-primary" @click.prevent="send">Изпрати</button>
-							<!-- <span slot="footer" class="dialog-footer">
-								<el-button @click="dialogVisible = false">Откажи</el-button>
-								<el-button type="primary" @click="send">Изпрати</el-button>
-							</span> -->
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- <div>
-		<a href="#" :class="btnClass" @click="dialogVisible = true">{{ buttonText }}</a>
-		<el-dialog
-			:title="'Запитване'"
-			:visible.sync="dialogVisible"
-			width="400px">
-			<div>
-				<form class="ui form">
-					<div class="field">
-						<template v-if="errors.from" v-for="error in errors.from">
-							 <el-alert type="error" :title="error"></el-alert>
-						</template>
-						<input type="text" v-model="form.from" name="from" placeholder="Лице за контакти">
-					</div>
-					<div class="field">
-						<template v-if="errors.email" v-for="error in errors.email">
-							 <el-alert type="error" :title="error"></el-alert>
-						</template>
-						<input type="email" v-model="form.email" name="email" placeholder="E-mail">
-					</div>
-					<div class="field">
-						<template v-if="errors.phone" v-for="error in errors.phone">
-							 <el-alert type="error" :title="error"></el-alert>
-						</template>
-						<input type="text" v-model="form.phone" name="phone" placeholder="Телефон">
-					</div>
-
-					<div class="field">
-						<template v-if="errors.subject" v-for="error in errors.subject">
-							 <el-alert type="error" :title="error"></el-alert>
-						</template>
-						<input type="text" v-model="form.subject" name="subject" placeholder="Относно">
-					</div>
-
-					<div class="field">
-						<template v-if="errors.body" v-for="error in errors.body">
-							 <el-alert type="error" :title="error"></el-alert>
-						</template>
-						<textarea name="body" v-model="form.body" placeholder="Запитване..." rows="4"></textarea>
-					</div>
-				</form>
-			</div>
-			<span slot="footer" class="dialog-footer">
-				<el-button @click="dialogVisible = false">Откажи</el-button>
-				<el-button type="primary" @click="send">Изпрати</el-button>
-			</span>
-		</el-dialog>
-	</div> -->
 </template>
 
 <script>
@@ -139,7 +87,7 @@
     			.catch(function (error) {
     				console.log(error.response);
     				vm.errors = error.response.data.errors;
-    				vm.closeModal();
+    				// vm.closeModal();
     			})
         	}
         },
