d3fd5b5b9ac033865db92b7b823ce4afc49116fd
bgcert <register@bgcert-training.com>
2018-12-17 19:29:36 +0200

    Live page improved.

--- resources/views/pages/live.blade.php
+++ resources/views/pages/live.blade.php
@@ -2,50 +2,91 @@
 
 @section('content')
 
-<div class="live-streaming-header">
-	
+<div class="live-streaming-header d-flex justify-content-center align-items-center">
+	<h1 class="text-white">Излъчвайте събитията си на живо</h1>
 </div>
 
 <div class="container">
 	<div class="text-center">
-		<h2 class="mt-3">Излъчвайте събитията си на живо</h2>
-		<p>Seminari365 предага видео стрийминг услуга ..</p>
-		<p>Професионален стрийминг на събития
-		Организирате курс или семинар? Достигнете до Вашата аудитория навсякъде!
-		Излъчвайте към клиенти по целия свят чрез Seminari365.com. 
-		Продавайте Вашите събития без ограничения!.
+		<h3 class="mt-4">Професионален стрийминг на събития</h3>
 
-		Ние гарантираме излъчване с най-високо качество от всяка точка на света.
-
-		ИЗПРАТЕТЕ ЗАПИТВАНЕ ИЛИ СЕ ОБАДЕТЕ НА 02/ 4032404</p>
+		<p class="my-4">
+			Организирате курс или семинар? Достигнете до Вашата аудитория навсякъде!
+			Излъчвайте към клиенти по целия свят чрез Seminari365.com. 
+			Продавайте Вашите събития без ограничения!
+		</p>
 
 		<div class="card-deck">
-			<div class="card">
-				<img class="card-img-top" src=".../100px200/" alt="Card image cap">
+			<div class="card border-success">
+				<div class="card-header bg-success text-white">
+					<h3>Старт 10</h3>
+				</div>
 				<div class="card-body">
-					<h5 class="card-title">Card title</h5>
+					<h1 class="card-title">
+						80
+						<small>лв./ден</small>
+					</h1>
+					<ul class="list-group list-group-flush mt-4">
+						<li class="list-group-item">До 10 online участника</li>
+						<li class="list-group-item">Ограничаване на достъпа до излъчването</li>
+						<li class="list-group-item">Възможност за запис</li>
+						<li class="list-group-item">Съдействие при настройване на системата</li>
+					</ul>
 					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-					<a href="#" class="btn btn-warning btn-lg btn-block">Заяви</a>
+					<a href="#" class="btn btn-success btn-lg btn-block">Заяви</a>
 				</div>
 			</div>
-			<div class="card">
-				<img class="card-img-top" src=".../100px200/" alt="Card image cap">
+
+			<div class="card border-primary">
+				<div class="card-header bg-primary text-white">
+					<h3>Про 20</h3>
+				</div>
 				<div class="card-body">
-					<h5 class="card-title">Card title</h5>
-					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<h1 class="card-title">
+						120 
+						<small>лв./ден</small>
+					</h1>
+					<ul class="list-group list-group-flush mt-4">
+						<li class="list-group-item">До 20 online участника</li>
+						<li class="list-group-item">Органичен достъп до излъчването</li>
+						<li class="list-group-item">Възможност за запис</li>
+						<li class="list-group-item">Съдействие при настройване на системата</li>
+					</ul>
+					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					<a href="#" class="btn btn-primary btn-lg btn-block">Заяви</a>
 				</div>
 			</div>
+
 			<div class="card">
-				<img class="card-img-top" src=".../100px200/" alt="Card image cap">
+				<div class="card-header">
+					<h3>Бизнес</h3>
+				</div>
 				<div class="card-body">
-					<h5 class="card-title">Card title</h5>
-					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					<h5 class="card-title">
+						ПОПИТАЙТЕ НА 02/4032404
+					</h5>
+					<ul class="list-group list-group-flush mt-4">
+						<li class="list-group-item">Неограничен брой участници</li>
+						<li class="list-group-item">Ограничаване на достъпа до излъчването</li>
+						<li class="list-group-item">Възможност за запис</li>
+						<li class="list-group-item">Транскодиране с различно качество</li>
+						<li class="list-group-item">Съдействие при настройване на системата</li>
+					</ul>
+					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
 			</div>
 		</div>
+
+		<h4 class="mt-4">
+			Ние гарантираме излъчване с най-високо качество от всяка точка на света.
+		</h4>
+
+		<p>
+			ИЗПРАТЕТЕ ЗАПИТВАНЕ ИЛИ СЕ ОБАДЕТЕ НА 02/ 4032404
+		</p>
 	</div>
 </div>
 
